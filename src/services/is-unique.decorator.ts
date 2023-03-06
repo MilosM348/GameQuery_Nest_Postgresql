85769623b4aed94e8334e4947eabfc02100e9ab7
MilosM348 <milos.milic001@outlook.com>
2023-03-06 15:43:04 +0100

    fix

--- src/services/is-unique.decorator.ts
+++ src/services/is-unique.decorator.ts
@@ -1,6 +1,17 @@
 import { registerDecorator, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
 import { getRepository } from 'typeorm';
 
+@ValidatorConstraint({ async: true })
+export class IsUniqueConstraint implements ValidatorConstraintInterface {
+  async validate(value: any, args: ValidationArguments) {
+    const entity = args.object;
+    const property = args.property;
+
+    const repository = getRepository(entity.constructor.name);
+    const existingRecord = await repository.findOne({ [property]: value });
+    return !existingRecord;
+  }
+}
 
 export function IsUnique(entity: Function, validationOptions?: ValidationOptions) {
   return function (object: Object, propertyName: string) {
--- src/services/swagger.ts
+++ src/services/swagger.ts
@@ -3,6 +3,15 @@ export interface SwaggerDocumentOptions {
    * List of modules to include in the specification
    */
   include?: Function[];
+
+  /**
+   * Additional, extra models that should be inspected and included in the specification
+   */
+  extraModels?: Function[];
+
+  /**
+   * If `true`, swagger will ignore the global prefix set through `setGlobalPrefix()` method
+   */
   ignoreGlobalPrefix?: boolean;
 
   /**
