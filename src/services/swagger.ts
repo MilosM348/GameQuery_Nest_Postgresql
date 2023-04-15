b97c5e17e7555796c0d90108ceb2b289522e2cd1
MilosM348 <milos.milic001@outlook.com>
2023-04-15 13:00:11 +0200

    update swagger

--- src/services/swagger.ts
+++ src/services/swagger.ts
@@ -30,10 +30,6 @@ export interface SwaggerDocumentOptions {
 export interface SwaggerCustomOptions {
   explorer?: boolean;
   swaggerOptions?: Record<string, any>;
-  customCss?: string;
-  customCssUrl?: string;
-  customJs?: string;
-  customfavIcon?: string;
   swaggerUrl?: string;
   customSiteTitle?: string;
   validatorUrl?: string;
